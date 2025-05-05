1b2b631855e8b129e3b24a7450f410a5634101dc
Haruna Nishita <h.nishita.1@gmail.com>
2025-05-05 20:37:27 +0900

    style: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-purple-200 p-4 sticky bottom-0 opacity-50 text-center">
+    <footer className="text-purple-200 h-16 p-4 sticky bottom-0 opacity-50 text-center">
       <p>© 2025 Haruna Nishita</p>
     </footer>
   );
